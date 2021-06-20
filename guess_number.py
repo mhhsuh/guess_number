72903642ee4591301a38f31a17a034772836ba99
MH <mhhsu6110@gmail.com>
2021-06-20 10:28:48 +0800

    final version of guess number

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,24 @@
 import random
-r = random.randint(1, 100)
+start = input ('please choose the start of the range: ')
+end = input('please choose the end of the range: ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 1
 while True:
-	count += 1    # count = count + 1
-	guess = input('please key in one number between 1-100: ')
+	guess = input('please key in one number between the range: ' )
 	guess = int(guess)
-	print('you have guess', count, 'time')
-	if r == guess:
-		print("Correct")
-		break
-	elif r > guess:
-		print('the number is bigger')
-	elif r < guess:
-		print('the number is smaller')
+	if guess <= end and guess >= start:
+		print('you have guess', count, 'time')
+		if r == guess:
+			print("Correct")
+			break
+		elif r > guess:
+			print('the number is bigger')
+		elif r < guess:
+			print('the number is smaller')
+		print('you have counted', count, 'time')
+		count += 1 # count = count + 1
+	else:
+		print('please key in the number within the range')
